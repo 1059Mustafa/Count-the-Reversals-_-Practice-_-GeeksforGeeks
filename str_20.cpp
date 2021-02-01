7e72bdf09d382d2b7c7d13b9a13d7f8ff7d6f281
1059Mustafa <72117500+1059Mustafa@users.noreply.github.com>
2021-02-01 11:52:35 +0530

    Update str_20.cpp

--- str_20.cpp
+++ str_20.cpp
@@ -1,1 +1,35 @@
-Count the Reversals _ Practice _ GeeksforGeeks
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--){
+	    string str;
+	    cin>>str;
+	    int l=str.size();
+	   
+	    int i=0,rev=0;
+	    stack<char> s;
+	    for(i=0;i<l;i++)
+	    {
+	        if(str[i]=='{') s.push(str[i]);
+	        else{
+	            if(s.empty()){
+	                s.push('{');
+	                rev++;
+	                        }
+	            else{
+	                s.pop();
+	                }
+	            }
+	    }
+	   
+	   if(!s.empty())
+	       rev=(s.size()%2==1)?-1:(rev+s.size()/2);
+	   cout<<rev<<endl;
+	    }
+	return 0;
+}
